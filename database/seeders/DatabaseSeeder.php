726d2830353bdf8b31fd6c88ed451989aa4380c8
francesco mansi <f.mansi@socialwebsolutions.it>
2025-08-05 17:13:40 +0200

    aggiunta file

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Book;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        //https://fakerphp.org/
+        User::factory()->create([
+            'name' => 'Francesco',
+            'password' => Hash::make('*&Oppassword'),
+            'email' => 'fra.mansi@gmail.com',
+            'is_admin' => true,
+        ]);
+
+        User::factory(100)->create();
+        Book::factory(1000)->create();
+    }
+}
